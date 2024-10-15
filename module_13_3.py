6c5698f009f531984e3e60e1f6a0be4c0fed8b74
V1m4v0 <almax.crazy2015@yandex.ru>
2024-10-15 17:05:12 +0300

    Create module_13_3.py

---
+++ module_13_3.py
@@ -0,0 +1,20 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = ""
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+@dp.message_handler(commands = ['start'])
+async def start(message):
+    print(f'Привет! Я бот помогающий твоему здоровью.')
+    await message.answer("Я нача    л работу, чем могу помочь?")
+
+@dp.message_handler()
+async def all_message(message):
+    print(f'Введите команду /start, чтобы начать общение.')
+    await message.answer("Для начала работы введите команду /start")
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
